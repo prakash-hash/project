1c015e6dc2e38cfe6885bcd672ab880fca91d385
Prakash <pk46833@gmail.com>
2021-12-06 00:11:23 +0530

    initial commit

---
+++ Project Code/Main1.m
@@ -0,0 +1,283 @@
+function varargout = Main1(varargin)
+
+gui_Singleton = 1;
+gui_State = struct('gui_Name',       mfilename, ...
+                   'gui_Singleton',  gui_Singleton, ...
+                   'gui_OpeningFcn', @Main1_OpeningFcn, ...
+                   'gui_OutputFcn',  @Main1_OutputFcn, ...
+                   'gui_LayoutFcn',  [] , ...
+                   'gui_Callback',   []);
+if nargin && ischar(varargin{1})
+    gui_State.gui_Callback = str2func(varargin{1});
+end
+
+if nargout
+    [varargout{1:nargout}] = gui_mainfcn(gui_State, varargin{:});
+else
+    gui_mainfcn(gui_State, varargin{:});
+end
+% End initialization code - DO NOT EDIT
+
+
+% --- Executes just before Main1 is made visible.
+function Main1_OpeningFcn(hObject, eventdata, handles, varargin)
+% This function has no output args, see OutputFcn.
+% hObject    handle to figure
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+% varargin   command line arguments to Main1 (see VARARGIN)
+
+% Choose default command line output for Main1
+handles.output = hObject;
+
+% Update handles structure
+guidata(hObject, handles);
+
+% UIWAIT makes Main1 wait for user response (see UIRESUME)
+% uiwait(handles.figure1);
+
+
+% --- Outputs from this function are returned to the command line.
+function varargout = Main1_OutputFcn(hObject, eventdata, handles) 
+% varargout  cell array for returning output args (see VARARGOUT);
+% hObject    handle to figure
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Get default command line output from handles structure
+varargout{1} = handles.output;
+
+
+% --- Executes on button press in pushbutton1.
+function pushbutton1_Callback(hObject, eventdata, handles)
+% hObject    handle to pushbutton1 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+global input
+[f p]=uigetfile('Dataset\*.jpg','Select An Image');
+input=imread([p f]);
+input=imresize(input,[256,256]);
+[a b c]=size(input);
+if c==3
+    input=rgb2gray(input);
+end
+axes(handles.axes1);
+imshow(input);
+title('Given Eye Image','fontsize',14,'fontname','Times New Roman','color','Black');
+axis off;
+
+% --- Executes on button press in pushbutton2.
+function pushbutton2_Callback(hObject, eventdata, handles)
+% hObject    handle to pushbutton2 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+global input
+global img1;
+img1 = medfilt2(input,[3,3]);
+axes(handles.axes2);
+imshow(img1);
+title('Preprocessed Eye Image','fontsize',14,'fontname','Times New Roman','color','Black');
+axis off;
+
+% --- Executes on button press in pushbutton3.
+function pushbutton3_Callback(hObject, eventdata, handles)
+% hObject    handle to pushbutton3 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+% axes(handles.axes1);
+
+global img1 
+global x;
+global y;
+% rmin=54;
+% rmax=74;
+rmin=90;
+rmax=120;
+[out,x,y]=thresh(img1,rmin,rmax);
+axes(handles.axes3);
+imshow(out);
+title('Detected Eye Lid','fontsize',14,'fontname','Times New Roman','color','Black');
+axis off;
+
+% --- Executes on button press in pushbutton4.
+function pushbutton4_Callback(hObject, eventdata, handles)
+% hObject    handle to pushbutton4 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+global input testimg sec1;
+global x;
+global y;
+[m n]=size(input);
+sec1=zeros(m,n);
+seg=poly2mask(x,y,m,n);
+sec1(seg)=input(seg);
+axes(handles.axes4);
+imshow(sec1,[]);
+title('Detected Eye Lid','fontsize',14,'fontname','Times New Roman','color','Black');
+axis off;
+
+% --- Executes on button press in pushbutton5.
+function pushbutton5_Callback(hObject, eventdata, handles)
+% hObject    handle to pushbutton5 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+
+% --- Executes on button press in pushbutton6.
+function pushbutton6_Callback(hObject, eventdata, handles)
+% hObject    handle to pushbutton6 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+global image1
+[file path]=uigetfile('Dataset\*.tif','Select An Image');
+image1=imread([path file]);
+image1=imresize(image1,[256,256]);
+[a b c]=size(image1);
+if c==3
+    image1=rgb2gray(image1);
+end
+axes(handles.axes5);
+imshow(image1);
+title('Given Fingerprint','fontsize',14,'fontname','Times New Roman','color','Black');
+ axis off;
+
+% --- Executes on button press in pushbutton7.
+function pushbutton7_Callback(hObject, eventdata, handles)
+% hObject    handle to pushbutton7 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+global image1 image2
+image2 = medfilt2(image1);
+axes(handles.axes6);
+imshow(image2);
+title('Preprocessed','fontsize',14,'fontname','Times New Roman','color','Black');
+axis off;
+
+
+
+% --- Executes on button press in pushbutton8.
+function pushbutton8_Callback(hObject, eventdata, handles)
+% hObject    handle to pushbutton8 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+global image2 image1 outImg Finger
+Ig=image2;
+binary_image=im2bw(Ig);
+%Small region is taken to show output clear
+binary_image = binary_image(1:250,60:230);
+%Thinning
+thin_image=~bwmorph(binary_image,'thin',Inf);
+axes(handles.axes7);
+imshow(thin_image);
+title('Thinning','fontsize',14,'fontname','Times New Roman','color','Black');
+axis off;
+
+%Minutiae extraction
+s=size(thin_image);
+N=3;%window size
+n=(N-1)/2;
+r=s(1)+2*n;
+c=s(2)+2*n;
+double temp(r,c);   
+temp=zeros(r,c);bifurcation=zeros(r,c);ridge=zeros(r,c);
+temp((n+1):(end-n),(n+1):(end-n))=thin_image(:,:);
+outImg=zeros(r,c,3);%For Display
+outImg(:,:,1) = temp .* 255;
+outImg(:,:,2) = temp .* 255;
+outImg(:,:,3) = temp .* 255;
+for x=(n+1+10):(s(1)+n-10)
+    for y=(n+1+10):(s(2)+n-10)
+        e=1;
+        for k=x-n:x+n
+            f=1;
+            for l=y-n:y+n
+                mat(e,f)=temp(k,l);
+                f=f+1;
+            end
+            e=e+1;
+        end;
+         if(mat(2,2)==0)
+            ridge(x,y)=sum(sum(~mat));
+            bifurcation(x,y)=sum(sum(~mat));
+         end
+    end;
+end;
+
+% RIDGE END FINDING
+[ridge_x ridge_y]=find(ridge==2);
+len=length(ridge_x);
+%For Display
+for i=1:len
+    outImg((ridge_x(i)-3):(ridge_x(i)+3),(ridge_y(i)-3),2:3)=0;
+    outImg((ridge_x(i)-3):(ridge_x(i)+3),(ridge_y(i)+3),2:3)=0;
+    outImg((ridge_x(i)-3),(ridge_y(i)-3):(ridge_y(i)+3),2:3)=0;
+    outImg((ridge_x(i)+3),(ridge_y(i)-3):(ridge_y(i)+3),2:3)=0;
+    
+    outImg((ridge_x(i)-3):(ridge_x(i)+3),(ridge_y(i)-3),1)=255;
+    outImg((ridge_x(i)-3):(ridge_x(i)+3),(ridge_y(i)+3),1)=255;
+    outImg((ridge_x(i)-3),(ridge_y(i)-3):(ridge_y(i)+3),1)=255;
+    outImg((ridge_x(i)+3),(ridge_y(i)-3):(ridge_y(i)+3),1)=255;
+end
+
+%BIFURCATION FINDING
+[bifurcation_x bifurcation_y]=find(bifurcation==4);
+len=length(bifurcation_x);
+%For Display
+for i=1:len
+    outImg((bifurcation_x(i)-3):(bifurcation_x(i)+3),(bifurcation_y(i)-3),1:2)=0;
+    outImg((bifurcation_x(i)-3):(bifurcation_x(i)+3),(bifurcation_y(i)+3),1:2)=0;
+    outImg((bifurcation_x(i)-3),(bifurcation_y(i)-3):(bifurcation_y(i)+3),1:2)=0;
+    outImg((bifurcation_x(i)+3),(bifurcation_y(i)-3):(bifurcation_y(i)+3),1:2)=0;
+    
+    outImg((bifurcation_x(i)-3):(bifurcation_x(i)+3),(bifurcation_y(i)-3),3)=255;
+    outImg((bifurcation_x(i)-3):(bifurcation_x(i)+3),(bifurcation_y(i)+3),3)=255;
+    outImg((bifurcation_x(i)-3),(bifurcation_y(i)-3):(bifurcation_y(i)+3),3)=255;
+    outImg((bifurcation_x(i)+3),(bifurcation_y(i)-3):(bifurcation_y(i)+3),3)=255;
+end
+axes(handles.axes8);
+imshow(outImg);
+Finger=outImg;
+outImg=rgb2gray(outImg);
+testfea2=mean(outImg);
+save testfea2 testfea2;
+title('Minutae Extraction','fontsize',14,'fontname','Times New Roman','color','Black');
+axis off;
+
+
+
+% --- Executes on button press in pushbutton9.
+function pushbutton9_Callback(hObject, eventdata, handles)
+% hObject    handle to pushbutton9 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+
+
+% --- Executes on button press in pushbutton10.
+function pushbutton10_Callback(hObject, eventdata, handles)
+% hObject    handle to pushbutton10 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+
+
+% --- Executes on button press in pushbutton12.
+function pushbutton12_Callback(hObject, eventdata, handles)
+% hObject    handle to pushbutton12 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+
+
+
+% --- Executes on button press in pushbutton11.
+function pushbutton11_Callback(hObject, eventdata, handles)
+% hObject    handle to pushbutton11 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% --- Executes on button press in pushbutton13.
+function pushbutton13_Callback(hObject, eventdata, handles)
+% hObject    handle to pushbutton13 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
---
+++ Project Code/thresh.m
@@ -0,0 +1,52 @@
+
+function [out,x1,y1]=thresh(I,rmin,rmax);
+scale=1;
+
+rmin=rmin*scale;
+rmax=rmax*scale;
+
+I=im2double(I);
+
+pimage=I;
+
+I=imresize(I,scale);
+I=imcomplement(imfill(imcomplement(I),'holes'));
+
+rows=size(I,1);
+cols=size(I,2);
+[X,Y]=find(I<0.5);
+
+s=size(X,1);
+for k=1:s 
+    if (X(k)>rmin)&(Y(k)>rmin)&(X(k)<=(rows-rmin))&(Y(k)<(cols-rmin))
+            A=I((X(k)-1):(X(k)+1),(Y(k)-1):(Y(k)+1));
+            M=min(min(A));
+          
+           if I(X(k),Y(k))~=M
+              X(k)=NaN;
+              Y(k)=NaN;
+           end
+    end
+end
+v=find(isnan(X));
+X(v)=[];
+Y(v)=[];
+index=find((X<=rmin)|(Y<=rmin)|(X>(rows-rmin))|(Y>(cols-rmin)));
+X(index)=[];
+Y(index)=[];  
+N=size(X,1);
+maxb=zeros(rows,cols);
+maxrad=zeros(rows,cols);
+for j=1:N
+    [b,r,blur]=partiald(I,[X(j),Y(j)],rmin,rmax,'inf',600,'iris');
+    maxb(X(j),Y(j))=b;
+    maxrad(X(j),Y(j))=r;
+end
+[x,y]=find(maxb==max(max(maxb)));
+ci=search(I,rmin,rmax,x,y,'iris');
+ci=ci/scale;
+cp=search(I,round(0.1*r),round(0.8*r),ci(1)*scale,ci(2)*scale,'pupil');
+cp=cp/scale;
+[out x1 y1]=drawcircle(pimage,[ci(1),ci(2)],ci(3),600);
+
+[out x y] =drawcircle(out,[cp(1),cp(2)],cp(3),600);
